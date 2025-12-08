dafc306b8a4a811c5e5f976870e1507aa49dd9a9
Maciej <maciej.polowy1@gmail.com>
2025-12-08 10:58:58 +0100

    fix redirect?

--- lib/actions.ts
+++ lib/actions.ts
@@ -93,7 +93,7 @@ export async function login(
     email,
     options: {
       shouldCreateUser: true,
-      emailRedirectTo: `${process.env.APP_URL}/auth/confirm`,
+      emailRedirectTo: encodeURI(`${process.env.APP_URL}/auth/confirm`),
     },
   });
 
